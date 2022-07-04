6f90419194f88ff50c53b72edc74e5b055c9be4c
WDominic <dom0_o@protonmail.com>
2022-07-04 17:15:57 -0400

    works kinda on my desktop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
        { field: 'NetProfit' }
    ])
    useEffect(() => {
-     fetch("http://127.0.0.1:5000/api/stonksdata").then((res) =>
+     fetch("/api/stonksdata").then((res) =>
      res.json()
      .then((data) => {
        setRowData(data)
